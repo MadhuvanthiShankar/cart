1712aab63a6ba45ee887021bd265f956eac59a0b
Madhu <madhuvanthishankar@gmail.com>
2024-01-02 15:26:58 +0530

    added images for the cards

--- src/components/Card/CardComp.jsx
+++ src/components/Card/CardComp.jsx
@@ -1,7 +1,20 @@
 import React from "react";
+import Card from "@mui/material/Card";
+import { CardMedia } from "@mui/material";
 
 const CardComp = () => {
-  return <div>CardComp</div>;
+  return (
+    <React.Fragment>
+      <Card sx={{ maxWidth: 345 }}>
+        <CardMedia
+          component="img"
+          alt="watermelon"
+          height="140"
+          image="/src/assets/watermelon.png"
+        />
+      </Card>
+    </React.Fragment>
+  );
 };
 
 export default CardComp;
